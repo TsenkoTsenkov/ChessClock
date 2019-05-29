e0b972a3591ea676b3d78b4bbbee6825f49b3b96
Kristian-A <kristian.zh.apostolov@gmail.com>
2019-05-29 11:49:52 +0300

    Added displaying of digits

---
+++ sketch_lcd_test/sketch_lcd_test.ino
@@ -0,0 +1,187 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printByte(args)  write(args);
+#else
+#define printByte(args)  print(args,BYTE);
+#endif
+
+uint8_t segments[8][8]  = {
+  {
+    B00111,
+    B01111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111
+  },
+
+  {
+    B11100,
+    B11110,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111
+  },
+
+  {
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11110,
+    B11100
+  },
+
+  {
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B01111,
+    B00111
+  },
+
+  {
+    B11111,
+    B11111,
+    B11111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000
+  },
+
+  {
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B11111,
+    B11111,
+    B11111
+  },
+
+  {
+    B11111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B11111
+  },
+
+  {
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111
+  }
+};
+
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  for (int i = 0; i< 8; i++) {
+    lcd.createChar(i, segments[i]);
+  }
+  lcd.home(); 
+//  drawFactory(0, 0);
+//  drawFactory(1, 3);
+//  drawFactory(2, 6); 
+//  drawFactory(3, 9);
+//  drawFactory(4, 12);
+  drawFactory(1, 0);
+  drawFactory(5, 3);
+  drawFactory(6, 6);
+  drawTwoDots(9);
+  drawFactory(8, 10); 
+  drawFactory(9, 13); 
+}
+
+void loop() {
+  clearSquare(3);
+  delay(100);
+  drawFactory(5, 3);
+  delay(600);
+}
+
+int zero[6]  = {0, 4, 1, 3, 5, 2};
+int one[6]   = {4, 1, -1, 5, 7, 5};
+int two[6]   = {4, 6, 1, 0, 6, 5};
+int three[6] = {4, 6, 1, 5, 6, 2};
+int four[6]  = {3, 5, 7, -1, -1, 7};
+int five[6]  = {0, 6, 4, 5, 6, 2};
+int six[6]   = {0, 6, 4, 3, 6, 2};
+int seven[6] = {4, 4, 2, -1, 0, -1};
+int eight[6] = {0, 6, 1, 3, 6, 2};
+int nine[6]  = {0, 6, 1, 5, 6, 2};
+
+void drawFactory(int num, int x){
+  switch(num){
+    case 0: drawNum(zero, x); break;
+    case 1: drawNum(one, x); break;
+    case 2: drawNum(two, x); break;
+    case 3: drawNum(three, x); break;
+    case 4: drawNum(four, x); break;
+    case 5: drawNum(five, x); break;
+    case 6: drawNum(six, x); break;
+    case 7: drawNum(seven, x); break;
+    case 8: drawNum(eight, x); break;
+    case 9: drawNum(nine, x); break;
+  }
+}
+
+void drawTwoDots(int x) {
+  lcd.setCursor(x, 0);
+  lcd.write(5);
+  lcd.setCursor(x, 1);
+  lcd.write(5);  
+}
+
+void clearSquare(int x) {
+  lcd.setCursor(x, 0);
+  for (int i = 0; i < 6; i++) {
+    lcd.print(" ");
+    if (i == 2) {
+      lcd.setCursor(x, 1);
+    } 
+  }  
+}
+void drawNum(int segs[], int x){
+  lcd.setCursor(x, 0);
+  for (int i = 0; i < 6; i++) {
+    if (segs[i] == -1) {
+      lcd.print(" ");
+    } else {
+      lcd.write(segs[i]);
+    }
+    if (i == 2) {
+      lcd.setCursor(x, 1);
+    }
+  }
+}
